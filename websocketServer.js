aeef1e3b809b17a153137998ada877ab317ce0dc
Brandon Henning <prescott.henning@gmail.com>
2018-08-29 13:17:53 -0600

    a

--- websocketServer.js
+++ websocketServer.js
@@ -4,7 +4,7 @@ const priceStore = require('./orderbook/pricestore')
 const log = console.log
 const chalk = require('chalk')
 
-const wss = new WebSocket.Server({ port: 4545 })
+const wss = new WebSocket.Server({ port: 4545 || process.env.PORT })
 
 wss.on('connection', function connection(ws) {
   ws.send(`${JSON.stringify(priceStore.data)}`)
