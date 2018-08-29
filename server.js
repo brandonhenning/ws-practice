fbf2795965845afd890ab597b7ba3efc223660b3
Brandon Henning <prescott.henning@gmail.com>
2018-08-29 14:42:28 -0600

    a

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const engine = require('./engine')
 const express = require('express')
 const app = express()
 const port = 3000 || process.env.PORT
-console.log(port)
 app.listen(port)
+console.log(process.env.PORT)
 
 engine.start()
 
